573a4ac09486302d2f7ef146edcd656706e73a22
Brayan Dadick <dadick.monzon@pucp.edu.pe>
2020-06-27 12:08:32 -0500

    validacion de sensor posicion

--- app/src/main/java/pucp/telecom/moviles/lab3/MainActivity.java
+++ app/src/main/java/pucp/telecom/moviles/lab3/MainActivity.java
@@ -1,8 +1,21 @@
 package pucp.telecom.moviles.lab3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.accessibilityservice.AccessibilityService;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +23,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        obtenerUbicacion();
+
+    }
+
+
+    public void obtenerUbicacion() {
+        int permiso =
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permiso == PackageManager.PERMISSION_GRANTED) {
+            FusedLocationProviderClient fusedLocationProviderClient =
+                    LocationServices.getFusedLocationProviderClient(this);
+
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    String  latitud= String.valueOf(location.getLatitude());
+                    String  longitud= String.valueOf(location.getLongitude());
+                }
+            });
+
+
+            fusedLocationProviderClient.getLastLocation().addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+
+                }
+            });
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //Toast.makeText(this,"Si se tiene permisos de posición",Toast.LENGTH_SHORT).show();
+                obtenerUbicacion();
+            } else {
+                //Toast.makeText(this,"No se tiene permisos de posición",Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
+
